9666e8d64597f292a68e7756c94ddf4feaba4c04
Fortis <fortispotens@gmail.com>
2021-01-16 22:53:12 +0100

    update App.js and rename to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './app.css';
+import './App.css';
 import Cards from './components/Cards';
 
 function App() {
