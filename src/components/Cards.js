f5fa321418f5d87b20d0a66bf122b9e06200f575
Fortis <fortispotens@gmail.com>
2021-01-17 18:47:20 +0100

    implement landscape orientation media query in header on  mobile view

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -19,7 +19,7 @@ const Cards = ({ data }) => {
 
 	return (
 		<div className='app-container bg-light'>
-			<div className='controls-container fixed-top bg-gradient-info text-light'>
+			<div className='controls-container fixed-top text-light'>
 				<div className='records-control  p-2 '>
 					<span className='app-title text-center text-light'>User Records</span>
 					<SearchBox
